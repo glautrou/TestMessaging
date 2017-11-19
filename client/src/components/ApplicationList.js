b469311c45cccb7d672492fa439dc5cbf716f680
glautrou <g.lautrou@outlook.com>
2017-11-19 17:43:12 +0100

    Create static application list

--- client/src/components/ApplicationList.js
+++ client/src/components/ApplicationList.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
-import { PageHeader } from 'react-bootstrap';
+import { PageHeader, Table } from 'react-bootstrap';
 
 class ApplicationList extends Component {
   render() {
     return (
-        <div>
-          <PageHeader>Applications</PageHeader>
-            
-        </div>
-      );
+      <div>
+        <PageHeader>Applications</PageHeader>
+
+        <Table responsive striped bordered condensed hover>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Creation date</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Application 1</td>
+              <td>18/11/2017</td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>Application 2</td>
+              <td>19/11/2017</td>
+            </tr>
+          </tbody>
+        </Table>
+      </div>
+    );
   }
 }
 
