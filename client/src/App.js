cd3dd77948196d7fcf16a1b3ff7bcbe42b9eb537
glautrou <g.lautrou@outlook.com>
2017-11-19 16:30:55 +0100

    Create basic page routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Home from './components/Home';
+import About from './components/About';
 
 class App extends Component {
   render() {
@@ -9,12 +11,23 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Messaging</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <Router>
+            <div>
+              <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+              </ul>
+
+              <hr />
+              <Route exact path="/" component={Home} />
+              <Route path="/about" component={About} />
+            </div>
+          </Router>
+
         </p>
-        <Home />
       </div>
     );
   }
