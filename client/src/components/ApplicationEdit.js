3fe9d632123fc5d2bd4b8ef7bf0fd82180ffb974
glautrou <g.lautrou@outlook.com>
2017-11-19 19:35:31 +0100

    Create basic form for creating an applicaation

--- client/src/components/ApplicationEdit.js
+++ client/src/components/ApplicationEdit.js
@@ -1,25 +1,80 @@
 import React, { Component } from 'react';
-import { Button, Modal } from 'react-bootstrap';
+import { Button, Modal, FormGroup, FormControl, ControlLabel, HelpBlock } from 'react-bootstrap';
 
 class ApplicationEdit extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      name: '',
+      isSubmiting: false
+    };
+
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  };
+
+  getValidationState() {
+    //Validate name
+    const length = this.state.name.length;
+    if (length > 10) return 'success';
+    else if (length > 5) return 'warning';
+    else if (length > 0) return 'error';
+    return null;
+  };
+
+  handleNameChange(e) {
+    this.setState({ name: e.target.value });
+  };
+
+  handleSubmit(e) {
+    this.setState({ isSubmiting: true });
+    //alert('submit: ' + this.state.name);
+    setTimeout(() => {
+      // Completed of async action, set loading state back
+      this.setState({ isSubmiting: false });
+    }, 2000);
+  };
+
   render() {
     return (
       <div>
+
         <Modal show={this.props.show} onHide={this.props.close}>
           <Modal.Header closeButton>
             <Modal.Title>New application</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <h4>Text in a modal</h4>
-            <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula.</p>
-            <hr />
-            <h4>Note</h4>
-            <p>After saving your application will be able to use specified Messsaging features.</p>
+            <form onSubmit={this.handleSubmit}>
+              <FormGroup
+                controlId="formBasicText"
+                validationState={this.getValidationState()}
+              >
+                <ControlLabel>Name</ControlLabel>
+                <FormControl
+                  type="text"
+                  value={this.state.name}
+                  placeholder="Enter text"
+                  onChange={this.handleNameChange}
+                />
+                <FormControl.Feedback />
+                <HelpBlock>Meaningful applicayion name. Minimum 5 characters.</HelpBlock>
+              </FormGroup>
+              <hr />
+              <h4>Note</h4>
+              <p>After saving your application will be able to use specified Messsaging features.</p>
+            </form>
           </Modal.Body>
           <Modal.Footer>
             <Button onClick={this.props.close}>Close</Button>
+            <Button type='submit' bsStyle='primary'
+              onClick={this.handleSubmit}
+              disabled={this.state.isSubmiting}>
+              {!this.state.isSubmiting ? 'Create' : 'Creating...'}
+            </Button>
           </Modal.Footer>
         </Modal>
+
       </div>
     );
   }
